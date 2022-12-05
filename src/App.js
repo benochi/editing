f6c725eb3720dea7317389c5615eaa1536f32336
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-12-05 07:23:44 -0600

    Clean up

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Profile from './components/Profile';
 function App() {
   return (
     <div className="App">
-      <h1>Edit demo</h1>
+      <h1>Edit form demo</h1>
       <Profile />
     </div>
   );
