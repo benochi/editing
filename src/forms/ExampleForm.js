0ffa6373068fc29c448ebed28b644100a449d895
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-12-05 07:33:45 -0600

    Updated form from using array method to names

--- src/forms/ExampleForm.js
+++ src/forms/ExampleForm.js
@@ -7,19 +7,19 @@ import {
 export default function ExampleForm({editClick, setUsername, setAlerts}) {
   function handleSubmit(e){
     e.preventDefault();
-    if(e.target[0].value.length > 0){
-      setUsername(e.target[0].value)
+    if(e.target.username.value.length > 0){
+      setUsername(e.target.username.value)
     }
-    if(e.target[1].value > 0){
-      setAlerts(e.target[1].value)
+    if(e.target.alerts.value > 0){
+      setAlerts(e.target.alerts.value)
     }
     editClick()
   }
 
   return (
     <form className="d-grid p-2 justify-content-center" onSubmit={handleSubmit}>
-      <MDBInput className='mb-4' type='username' id='form1Example1' label='Username' />
-      <MDBInput className='mb-4' type='alerts' id='form1Example1' label='# of Alerts' />
+      <MDBInput className='mb-4' type='username' name='username' id='form1Example1' label='Username' />
+      <MDBInput className='mb-4' type='alerts' id='form1Example1' name="alerts" label='# of Alerts' />
       <MDBBtn type='submit' block>
         Finish Edit
       </MDBBtn>
