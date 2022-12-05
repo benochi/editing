f8c0a910bd858b5e6bf2c8441aea2ea2a0a1e3dd
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-12-05 06:55:57 -0600

    editform done

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,49 @@
 import ExampleForm from "../forms/ExampleForm";
-import React from "react";
-import { MDBBadge, MDBBtn } from 'mdb-react-ui-kit';
+import { React, useState } from "react";
+import { MDBBadge, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
 
 export default function Profile(){
+  const [username, setUsername] = useState("Bob")
+  const [alerts, setAlerts] = useState(4)
+  const [editing, setEditing] = useState(true)
 
+  function alertClick(){
+    setAlerts(alerts + 1)
+  }
+
+  function editClick(){
+    setEditing(!editing)
+  }
 
   return (
+    <div>
+    {editing ?
     <div className="d-flex p-2 justify-content-center">
-      <MDBBtn>
-      Profile
-      <MDBBadge className='ms-2' color='danger'>
-        9
-      </MDBBadge>
-      <span className='visually-hidden'>unread messages</span>
-    </MDBBtn>
+      <MDBTypography tag='h3' className='mb-0'>
+        User: {username}<br></br>
+        <MDBTypography tag='small' className='text-muted'>
+          With faded secondary text
+        </MDBTypography>
+      </MDBTypography>
+      <MDBBtn className="m-2" onClick={alertClick}>
+        Alerts
+        <MDBBadge className='ms-2' color='danger'>
+          {alerts}
+        </MDBBadge>
+        <span className='visually-hidden'>unread messages</span>
+      </MDBBtn>
+      <MDBBtn className="m-2" onClick={editClick}>
+        EDIT
+        <MDBBadge className='ms-2' color='danger'>
+          {editing}
+        </MDBBadge>
+        <span className='visually-hidden'>unread messages</span>
+      </MDBBtn>
+    </div>
+    : <ExampleForm 
+        editClick={editClick}
+        setUsername={setUsername}
+      />}
     </div>
   );
 }
--- src/forms/ExampleForm.js
+++ src/forms/ExampleForm.js
@@ -7,23 +7,20 @@ import {
   MDBBtn
 } from 'mdb-react-ui-kit';
 
-export default function ExampleForm() {
+export default function ExampleForm({editClick, setUsername}) {
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(e.target[0].value)
+    setUsername(e.target[0].value)
+    editClick()
+
+  }
+
   return (
-    <form>
-      <MDBInput className='mb-4' type='email' id='form1Example1' label='Email address' />
-      <MDBInput className='mb-4' type='password' id='form1Example2' label='Password' />
-
-      <MDBRow className='mb-4'>
-        <MDBCol className='d-flex justify-content-center'>
-          <MDBCheckbox id='form1Example3' label='Remember me' defaultChecked />
-        </MDBCol>
-        <MDBCol>
-          <a href='#!'>Forgot password?</a>
-        </MDBCol>
-      </MDBRow>
-
+    <form className="d-grid p-2 justify-content-center" onSubmit={handleSubmit}>
+      <MDBInput className='mb-4' type='username' id='form1Example1' label='Username' />
       <MDBBtn type='submit' block>
-        Sign in
+        Finish Edit
       </MDBBtn>
     </form>
   );
