61340d87b443ce3ccc095a6f82d515c308c6f61a
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-12-05 07:21:05 -0600

    Added alert dropdown

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,19 +1,15 @@
 import ExampleForm from "../forms/ExampleForm";
 import { React, useState } from "react";
-import { MDBBadge, MDBBtn, MDBTypography } from 'mdb-react-ui-kit';
+import { MDBBadge, MDBBtn, MDBTypography, MDBCollapse } from 'mdb-react-ui-kit';
 
 export default function Profile(){
   const [username, setUsername] = useState("Bob")
   const [alerts, setAlerts] = useState(4)
   const [editing, setEditing] = useState(true)
+  const [showShow, setShowShow] = useState(false);
 
-  function alertClick(){
-    setAlerts(alerts + 1)
-  }
-
-  function editClick(){
-    setEditing(!editing)
-  }
+  const toggleShow = () => setShowShow(!showShow);
+  const editClick = () => setEditing(!editing)
 
   return (
     <div>
@@ -25,24 +21,32 @@ export default function Profile(){
           Totally the most L337 user
         </MDBTypography>
       </MDBTypography>
-      <MDBBtn className="m-2" onClick={alertClick}>
-        Alerts
-        <MDBBadge className='ms-2' color='danger'>
-          {alerts}
-        </MDBBadge>
-        <span className='visually-hidden'>unread messages</span>
-      </MDBBtn>
+        <MDBBtn className="m-2 col-1" onClick={toggleShow}>
+          Alerts
+          <MDBBadge className='ms-2' color='danger'>
+            {alerts}
+          </MDBBadge>
+          <span className='visually-hidden'>unread messages</span>
+          <div className='d-flex justify-content-center flex-sm-fill'>
+            <MDBCollapse show={showShow} className="bg-white text-black p-2 m-2">
+              This is where alerts could appear. lets see how this formats as I click on it!
+              You could put alerts into a list and cycle through them, or add arrows to the button and move up and down through the list!  
+            </MDBCollapse>
+          </div>
+        </MDBBtn>  
+      <div>
       <MDBBtn className="m-2" onClick={editClick}>
         EDIT
-        <MDBBadge className='ms-2' color='danger'>
+        <MDBBadge className='' color='danger'>
           {editing}
         </MDBBadge>
-        <span className='visually-hidden'>unread messages</span>
       </MDBBtn>
+      </div>
     </div>
     : <ExampleForm 
         editClick={editClick}
         setUsername={setUsername}
+        setAlerts={setAlerts}
       />}
     </div>
   );
--- src/forms/ExampleForm.js
+++ src/forms/ExampleForm.js
@@ -4,16 +4,18 @@ import {
   MDBBtn
 } from 'mdb-react-ui-kit';
 
-export default function ExampleForm({editClick, setUsername}) {
+export default function ExampleForm({editClick, setUsername, setAlerts}) {
   function handleSubmit(e){
     e.preventDefault();
     setUsername(e.target[0].value)
+    setAlerts(e.target[1].value)
     editClick()
   }
 
   return (
     <form className="d-grid p-2 justify-content-center" onSubmit={handleSubmit}>
       <MDBInput className='mb-4' type='username' id='form1Example1' label='Username' />
+      <MDBInput className='mb-4' type='alerts' id='form1Example1' label='Alerts' />
       <MDBBtn type='submit' block>
         Finish Edit
       </MDBBtn>
