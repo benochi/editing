3a2e4b9f5beeb9e35b801b8892bd5fafc9a28b3d
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-12-05 07:25:48 -0600

    updated form label for accuracy

--- src/forms/ExampleForm.js
+++ src/forms/ExampleForm.js
@@ -15,7 +15,7 @@ export default function ExampleForm({editClick, setUsername, setAlerts}) {
   return (
     <form className="d-grid p-2 justify-content-center" onSubmit={handleSubmit}>
       <MDBInput className='mb-4' type='username' id='form1Example1' label='Username' />
-      <MDBInput className='mb-4' type='alerts' id='form1Example1' label='Alerts' />
+      <MDBInput className='mb-4' type='alerts' id='form1Example1' label='# of Alerts' />
       <MDBBtn type='submit' block>
         Finish Edit
       </MDBBtn>
